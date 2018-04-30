3405311171b11fae4ed2d4feb5b889d7283bb87d
ayush <mail4ayushmahajan@gmail.com>
2018-04-30 23:47:31 +0530

    Update wget.sh

--- wget.sh
+++ wget.sh
@@ -2,3 +2,8 @@ echo enter new folder nameL
 read a
 mkdir $a
 echo name enter webpage link:
+
+
+function validate_url(){
+  if [[ `wget -S --spider $1  2>&1 | grep 'HTTP/1.1 200 OK'` ]]; then echo "true"; fi
+}
