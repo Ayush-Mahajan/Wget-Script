d6488faa5248d63e18a826c40ee813e03adb1eb2
ayush <mail4ayushmahajan@gmail.com>
2018-05-01 14:39:22 +0530

    Update wget.sh

--- wget.sh
+++ wget.sh
@@ -1,6 +1,11 @@
 
 echo **********WELCOME**********
 INPUT_STRING=hello
+
+
+#Log file of the program
+LOGFILE=wget.log
+
 while [ "$INPUT_STRING" != "bye" ]
     do
         echo Enter folder name :
@@ -13,3 +18,41 @@ while [ "$INPUT_STRING" != "bye" ]
         echo "Type "bye" to quit"
         read INPUT_STRING
     done
+
+
+
+#!/bin/sh
+
+#default directories
+wget=/usr/bin/wget
+tar=/bin/tar
+
+#exit f cannot find basic requirements
+if[! -x "$wget"]; then
+    echo "Cannot FInd wget... Exiting"
+    exit 1
+elif[! -x "$tar"]; then
+    echo "Cannot Find tar... Exiting"
+    exit 1
+fi
+
+#Outout Folder Name
+DIR=website'date+%m%d"'
+
+#log file
+LOGFILE=wget.log
+
+#Download url
+WEBSITEURL=http://www.thegreatestshowmanmovie.com
+
+#Change current working irectory
+cd $DIR
+#get the website and save it
+wget $WEBSITEURL --mirror --convert-links --adjust-extension --page-requisites --no-parent --no-clobber --restrict-file-ame=windows -o
+
+$LOGFILE
+
+#change the current directory back
+cd..
+
+# compress(tar and)
