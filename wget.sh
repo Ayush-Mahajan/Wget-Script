511a4dd1380687fc5fe72715ca3e2e8f42d0c132
ayush <mail4ayushmahajan@gmail.com>
2018-05-01 14:55:37 +0530

    Update wget.sh

--- wget.sh
+++ wget.sh
@@ -1,26 +1,59 @@
 
 echo **********WELCOME**********
+echo Enter website URL and see it later even if you are offline
 INPUT_STRING=hello
 
+#default directories
+wget=/usr/bin/wget
+tar=/bin/tar
+
+#exit f cannot find basic requirements
+if[! -x "$wget"]; then
+    echo "Cannot Find wget in your computer... Sorry cannt run this on your computer"
+    exit 1
+elif[! -x "$tar"]; then
+    echo "Cannot Find tar in cpmuter... cannot run this program. find a computer which has tar"
+    exit 1
+fi
 
 #Log file of the program
 LOGFILE=wget.log
 
 while [ "$INPUT_STRING" != "bye" ]
     do
+        # Loop entry point
+        # folder name is taken from the User
         echo Enter folder name :
         read a
         mkdir $a
         cd $a
+        
+        #webpage URL input
         echo enter webpage link :
         read b
-        wget --mirror --no-parent $b 
+        
+        #code to check the levels of website the user wants to download
+        echo enter the leves you want to download:
+        read c
+        
+        #code to check if the user wants to throttle down the speed or not
+        echo do you want to throttle down the net speed:
+        read ans
+        
+        # check the user input
+        if[$ans = 'y' || $ans = 'Y']
+            echo enter the speed limit:
+            read s
+            wget -r -|$c --limit-rate=$s --adjust-extension --no-clobber --no-parent $b
+        else
+            wget -r -|$c --mirror --adjust-extension --no-clobber --no-parent $b 
+        fi
         echo "Type "bye" to quit"
         read INPUT_STRING
     done
 
 
-
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 #!/bin/sh
 
 #default directories
